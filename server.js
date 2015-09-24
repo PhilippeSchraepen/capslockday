94e3b73bcbb1db28f8c54f552aac2707c3e2d8ae
Philippe Schraepen <philippe.schraepen@euri.com>
2015-09-24 12:43:57 +0200

    test

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var webpack = require('webpack');
+var path = require('path');
 var WebpackDevServer = require('webpack-dev-server');
 var env = require('./server/env');
 var devConfig = require('./webpack.config');
 var isProduction = env.NODE_ENV === 'production';
+var buildPath = path.resolve(__dirname, 'static');
+console.log(buildPath);
+var express = require('express');
 var app = express();
 
 if (!isProduction){
@@ -22,7 +26,9 @@ if (!isProduction){
     console.log('Listening at ' + env.API_URL + ':' + env.API_PORT);
   })
 } else {
-  app.listen(port, function () {
+  // point to the static assets
+  app.use(express.static(buildPath));
+  app.listen(env.API_PORT, function () {
     console.log('Environment: ' + env.NODE_ENV);
     console.log('Listening at ' + env.API_URL + ':' + env.API_PORT);
   });
