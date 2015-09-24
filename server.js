1da9e069b5e009c21e16afee04bcc4e2732e330a
Philippe Schraepen <philippe.schraepen@euri.com>
2015-09-24 12:33:32 +0200

    test

--- server.js
+++ server.js
@@ -2,19 +2,28 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var env = require('./server/env');
 var devConfig = require('./webpack.config');
+var isProduction = env.NODE_ENV === 'production';
+var app = express();
 
-new WebpackDevServer(webpack(devConfig),{
-  publicPath: './static/',
-  hot: true,
-  stats: {
-    colors: true
-  },
-  historyApiFallback: true
-}).listen(env.API_PORT, env.API_URL, function(err, result){
-  if (err){
-    console.log(err);
-    return;
-  }
-  console.log('Environment: ' + env.NODE_ENV);
-  console.log('Listening at ' + env.API_URL + ':' + env.API_PORT);
-})
+if (!isProduction){
+  new WebpackDevServer(webpack(devConfig),{
+    publicPath: './static/',
+    hot: true,
+    stats: {
+      colors: true
+    },
+    historyApiFallback: true
+  }).listen(env.API_PORT, env.API_URL, function(err, result){
+    if (err){
+      console.log(err);
+      return;
+    }
+    console.log('Environment: ' + env.NODE_ENV);
+    console.log('Listening at ' + env.API_URL + ':' + env.API_PORT);
+  })
+} else {
+  app.listen(port, function () {
+    console.log('Environment: ' + env.NODE_ENV);
+    console.log('Listening at ' + env.API_URL + ':' + env.API_PORT);
+  });
+}
