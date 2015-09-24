01fcab5ed033836f1d3f22e8a5a383261981c33c
Philippe Schraepen <philippe.schraepen@euri.com>
2015-09-24 13:09:03 +0200

    test

--- server.js
+++ server.js
@@ -14,20 +14,19 @@ if (!isProduction){
       colors: true
     },
     historyApiFallback: true
-  }).listen(env.API_PORT, env.API_URL, function(err, result){
+  }).listen(env.PORT, env.URL, function(err, result){
     if (err){
       console.log(err);
       return;
     }
     console.log('Environment: ' + env.NODE_ENV);
-    console.log('Listening at ' + env.API_URL + ':' + env.API_PORT);
+    console.log('Listening at ' + env.URL + ':' + env.PORT);
   })
 } else {
   // point to the static assets
   app.use(express.static(__dirname));
-  app.listen(env.HEROKU_PORT), function () {
+  app.listen(env.PORT, function(){
     console.log('Environment: ' + env.NODE_ENV);
-    console.log('Listening at ' + env.API_PORT);
-
+    console.log('Listening at ' + env.PORT);
   });
 }
--- server/env.js
+++ server/env.js
@@ -1,6 +1,5 @@
 module.exports = {
-  API_URL: process.env.API_URL || 'localhost',
-  API_PORT: process.env.API_PORT || '8000',
-  HEROKU_PORT: process.env.PORT || 5000,
+  URL: process.env.API_URL || 'localhost',
+  PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'local'
 };
